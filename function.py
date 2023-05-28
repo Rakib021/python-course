417d22094ebb36ad497d351de701bb1816df3a36
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-05-28 21:16:34 +0600

    modules

--- function.py
+++ function.py
@@ -1,13 +1,13 @@
 
-# def doubleIt(num):
-#     result = num*2
-#     print(result)
-# doubleIt(6)
+def doubleIt(num):
+    result = num*2
+    print(result)
+doubleIt(6)
 
 
-def sum(num1,num2):
-    result = num1 +num2
-    return result
+# def sum(num1,num2):
+#     result = num1 +num2
+#     return result
 
-total = sum(22,45)
-print(total)
+# total = sum(22,45)
+# print(total)
---
+++ modules.py
@@ -0,0 +1,4 @@
+from function import doubleIt 
+from default_args import * #access all function from default_args function
+
+result= doubleIt(12)
