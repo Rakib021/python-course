45d0aa8897e8eae105d4f7239fdabf6dec0a1e67
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-05-28 20:51:45 +0600

    built_in_function

---
+++ built_in_func.py
@@ -0,0 +1,12 @@
+highest = max(23,54,21,12,89,54,99)
+print("highest number :",highest)
+
+smallest=min(23,54,12,11,88,44,32,10,21)
+print("smallest : ",smallest)
+
+highestArr =([23,54,21,12,89,54,99])
+print("highest number inside the array : ",highestArr)
+print(len(highestArr))
+
+total = sum([23,54,21,12,89,54,99])
+print("total = ",total)
